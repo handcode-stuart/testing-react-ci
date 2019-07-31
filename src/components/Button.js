ed4e76fafa9a40c70a3d61a57fcddf0db607e86c
Stuart Morris <admin@handcode.co.uk>
2019-07-31 13:10:01 +0100

    Tests now passing

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Button = ({ text, onClick }) => <button onClick={onClick} />;
+const Button = ({ text, onClick }) => <button onClick={onClick}>{text}</button>;
 
 Button.propTypes = {
     text: PropTypes.string.isRequired,
